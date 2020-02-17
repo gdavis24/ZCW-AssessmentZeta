2acf76bb2249bea81f8a3c452fb2825a99465e9e
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-17 12:31:45 -0500

    started part 2 string utils

--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -11,6 +11,7 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return an array of strings, each element representative of a respective word in the sentence
      */
     public static String[] getWords(String sentence) {
+
         return null;
     }
 
@@ -21,7 +22,8 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+
+        return sentence.split(" ")[0];
     }
 
     /**
