6db78dca0c45c20e4ef6c6607d82db31f10ef13a
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-17 11:31:06 -0500

    3 of 5 array utils done

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -27,7 +27,20 @@ public class ArrayUtils {
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
     public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+        Object[] newArray = new Object[objectArray.length-1];
+        int curVal = 0;
+
+        for (int i = 0; i < objectArray.length; i++){
+            if (objectArray[i] == objectToRemove){
+
+            }
+            else {
+                newArray[curVal] = objectArray[i];
+                curVal++;
+            }
+        }
+        return newArray;
+
     }
 
     /**
@@ -36,7 +49,27 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
     public static Object getMostCommon(Object[] objectArray) {
-        return null;
+
+        int count = 1, tempCount;
+        Object popular = objectArray[0];
+        Object temp = 0;
+        for (int i = 0; i < (objectArray.length - 1); i++)
+        {
+            temp = objectArray[i];
+            tempCount = 0;
+            for (int j = 1; j < objectArray.length; j++)
+            {
+                if (temp == objectArray[j])
+                    tempCount++;
+            }
+            if (tempCount > count)
+            {
+                popular = temp;
+                count = tempCount;
+            }
+        }
+        return popular;
+
     }
 
 
@@ -46,7 +79,27 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
     public static Object getLeastCommon(Object[] objectArray) {
-        return null;
+
+        int count = 1, tempCount;
+        Object notPopular = objectArray[0];
+        Object temp = 0;
+        for (int i = 0; i < (objectArray.length - 1); i++)
+        {
+            temp = objectArray[i];
+            tempCount = 0;
+            for (int j = 1; j < objectArray.length; j++)
+            {
+                if (temp == objectArray[j])
+                    tempCount++;
+            }
+            if (tempCount <= count)
+            {
+                notPopular = temp;
+                count = tempCount;
+            }
+        }
+        return notPopular;
+
     }
 
     /**
