54c808d20f34111f02f63c9ed1567d8d3490a083
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-19 13:35:11 -0500

    progress on palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,21 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        int numOfPalindromes = 0;
+        for (int i = 0; i < input.length(); i++){
+            for (int j = i; j < input.length(); j++){
+                if (input.charAt(i) == input.charAt(j)){
+                    numOfPalindromes++;
+
+                }
+                else {
+                    i++;
+                }
+                System.out.println(numOfPalindromes);
+            }
+        }
+
+        return numOfPalindromes;
+
     }
 }
